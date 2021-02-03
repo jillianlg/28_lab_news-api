bd3491e52afcf2b2834471638ff49ab0ecf001b8
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-03 15:08:41 -0800

    fixes error to SingleArticle model and prop types in Article.jsx

--- src/components/news/Article.jsx
+++ src/components/news/Article.jsx
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SingleArticle = ({ title, author, description }) => (
+  <div style={{ font: 'inherit', border: '1px solid black', padding: '8px' }}>
+    <h2>{title}</h2>
+    <h3>{author}</h3>  
+    <p style={{ fontSize: '10px' }}>{description}</p>
+  </div>
+);
+
+SingleArticle.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+export default SingleArticle;
